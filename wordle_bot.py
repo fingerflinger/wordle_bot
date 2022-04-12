268f5981ea8422bd9e59c0b514d82e8952d734f3
ericlvaughan <ericlvaughan+git@gmail.com>
2022-04-12 10:57:16 -0600

    Generate pattern from guesses

--- wordle_bot.py
+++ wordle_bot.py
@@ -65,25 +65,33 @@ def find_first_guess(wordlist):
 def pattern_from_int(pattern_i):
     pattern = [0]*5 
     digit_magnitudes = [3*3*3*3, 3*3*3, 3*3, 3, 1]
-    import pdb;pdb.set_trace()
     for i in range(5):
         pattern[i] = int(pattern_i / digit_magnitudes[i])
         pattern_i = pattern_i - pattern[i]*digit_magnitudes[i]
     return pattern 
 
-
+def pattern_from_guess(check_word, answer):
+    pattern = [0]*5
+    check_word = check_word.lower()
+    answer = answer.lower()
+    active_digits = [1, 1, 1, 1, 1] # We can only consider active digits, since position matters
+    for i in range(5):
+        if check_word[i] == answer[i]:
+            pattern[i] = 0
+            active_digits[i] = 0
+        elif check_word[i] in answer:
+            loc = answer.index(check_word[i])
+            pattern[i] = 1
+            active_digits[loc] = 0
+        else:
+            pattern[i] = 2
+    import pdb;pdb.set_trace()
+    
 def find_valid_subset(search_pattern, check_word, word_list):
+    # Find patterns for this word against the dictionary, and add to search_pattern index to collect metrics for each guess
     # Find word-list subset that matches the search pattern
     # if condition 1 && condition 2 && ..., then add to whitelist
-    for word in word_list:
-        active_digits = [1, 1, 1, 1, 1] # We can only consider active digits, since position matters
-        for i in range(5):
-            if search_pattern[i] == 0:
-                if word[i] != check_word[i]:
-                    break
-                active_digits[i] = 0 # Turn off the digit we just inspected
-            elif search_pattern[i] == 1:
-                # Loop remaining digits and check if in the word but wrong order 
+    pass
              
                 
 
@@ -91,7 +99,7 @@ def main():
     #import the word list
     with open("5letter_dict.txt", 'r') as fh:
         word_list = [line.rstrip() for line in fh]
-    
+    pattern_from_guess("found", "fonde")
     #populate a graph with 
 
 if __name__ == "__main__":
