a75114c75bdb61b8dad1ad2ae688845f84f5b201
ericlvaughan <ericlvaughan+git@gmail.com>
2022-04-14 11:55:52 -0600

    Added some tests, grinding through bugs

---
+++ test_wordle_bot.py
@@ -0,0 +1,19 @@
+import wordle_bot
+
+def test_find_idx_in_active_digits():
+    loc = wordle_bot.find_idx_in_active_digits('spear', [False, True, True, True, True], 'e')
+    assert(loc == 2)
+    loc = wordle_bot.find_idx_in_active_digits('spear', [False, True, False, True, True], 'e')
+    assert(loc == -1)
+    loc = wordle_bot.find_idx_in_active_digits('aares', [False, True, True, True, True], 'a')
+    assert(loc == 1)
+    loc = wordle_bot.find_idx_in_active_digits('araes', [False, True, True, True, True], 'a')
+    assert(loc == 2)
+    loc = wordle_bot.find_idx_in_active_digits('araes', [False, False, True, True, True], 'a')
+    assert(loc == 2)
+
+def test_pattern_from_guess():
+    pattern = wordle_bot.pattern_from_guess('lares', 'black')
+    assert(pattern == [1, 1, 2, 2, 2])
+    pattern = wordle_bot.pattern_from_guess('lares', 'royal')
+    assert(pattern == [1, 1, 1, 2, 2])
--- wordle_bot.py
+++ wordle_bot.py
@@ -1,3 +1,5 @@
+import pytest
+
 def extract_five_letter_words(dictionary_file_in, dictionary_file_out):
     with open(dictionary_file_in, 'r') as fh:
         lines = [line.rstrip() for line in fh]
@@ -34,34 +36,32 @@ def pattern_from_int(pattern_i):
         pattern_i = pattern_i - pattern[i]*digit_magnitudes[i]
     return pattern 
 
-
 def pattern_from_guess(check_word, answer):
     pattern = [-1]*5
+    # Kludgy but YOLO
     check_word = check_word.lower()
+    check_word_digits = [True]*5
     answer = answer.lower()
-    active_digits = [True] * 5 # We can only consider active digits, since position matters
+    answer_digits = [True]*5
+
     # Need to check in place matches first
     for i in range(5):
         if check_word[i] == answer[i]:
             pattern[i] = 0
-            active_digits[i] = False
+            answer_digits[i] = False
+            check_word_digits[i] = False
     for i in range(5):
-        # extract remaining letters
-        remaining = []
-        remaining_idx = []
-        for j in range(5):
-            if active_digits[j]:
-                remaining.append(answer[j])
-                remaining_idx.append(j)
-        if pattern[i] < 0:
-            # Is this letter in the remaining letters?
-            if check_word[i] in remaining:
-                loc = remaining.index(check_word[i])
-                pattern[i] = 1
-                active_digits[loc] = False
-            else:
-                pattern[i] = 2
-    return pattern 
+        if check_word_digits[i] == False:
+            # pattern is defined by check_word
+            continue
+        loc = find_idx_in_active_digits(answer, answer_digits, check_word[i])
+        if loc == -1:
+            pattern[i] = 2
+        else:
+            pattern[i] = 1
+            answer_digits[loc] = False
+
+    return pattern
 
 
 def int_from_pattern(pattern):
@@ -73,14 +73,24 @@ def int_from_pattern(pattern):
 
 
 def find_idx_in_active_digits(word, active_digits, letter):
-    loc = word.find(letter) # Need to search only active digits
+    loc = word.find(letter) # First occurance
+    loc_offset = 0
     if loc == -1:
+        return -1
+    if active_digits[loc] == True:
         return loc
-    while(active_digits[loc] == False):
-        loc = word[(loc+1):].find(letter) # Need to search only active digits
-        if loc == -1:
-            return loc
-    return loc
+    while(active_digits[loc+loc_offset] == False):
+        # How to handle 3-4 occurances of the same letter? Need to update loc
+        new_offset = word[(loc+loc_offset+1):].find(letter) # Need to search only active digits
+        if new_offset == -1:
+            # Eventually we will hit the end of the word and end up here
+            return -1
+        loc_offset = loc_offset + new_offset + 1 # + 1 here because -1 indicates fail 2 find
+        #if(word == "algor"):
+            #import pdb;pdb.set_trace()
+    
+    # We kick out of the loop once active_digits[loc+locoffset] is True
+    return loc+loc_offset
 
 def valid_subset(check_word, pattern, word_list):
     # TODO, there is a bug here that misses some edge cases
@@ -175,6 +185,7 @@ def calc_guess_n(word_list):
         if best_word_value < 0 or value < best_word_value:
             best_word = test_word
             best_word_value = value
+            print("{} {}".format(best_word, best_word_value))
     return best_word
 
 
@@ -182,18 +193,25 @@ def do_solve():
     # Guesses are only valid if on word list, so do not have to calculate exhaustive 5 letter sequences
     with open("5letter_dict.txt", 'r') as fh:
         word_list = [line.rstrip() for line in fh]
-    my_answer = "chunk"
+    '''
+    Challenges
+    Black - runtime? Stuck in a loop?
+    Royal - runtime? Stuck in a loop?
+    '''
+    my_answer = "royal"
     my_guess = "lares"
     subset = word_list 
 
     for i in range(5): # 5 guesses after the first
         # Make guess
-        print("Guess #{}".format(i+1))
+        print("Guess #{} is {}".format(i+1, my_guess))
         pattern = pattern_from_guess(my_guess, my_answer)
+        print_pattern(my_guess, pattern)
         if int_from_pattern(pattern) == 0:
             print("SUCCESS")
             break
         subset = valid_subset(my_guess, pattern, subset)
+        print("Remaining possible words: {}".format(len(subset)))
         my_guess = calc_guess_n(subset)
 
 
@@ -214,10 +232,20 @@ def interactive():
         subset = valid_subset(my_guess, pattern, subset)
         my_guess = calc_guess_n(subset)
         print("Here is my new guess, enter it {}".format(my_guess))
-        
+   
+def test_find_idx_in_active_digits():
+    find_idx_in_active_digits('spear', [False, True, True, True, True], 'e')
+
+def test_pattern_from_guess():
+    print(pattern_from_guess_2('lares', 'black'))
+    print(pattern_from_guess_2('lares', 'royal'))
+
+
 def main():
     #test_valid_subset()
     do_solve()
+    #test_pattern_from_guess()
+    #test_find_idx_in_active_digits()
     #interactive()
          
     
